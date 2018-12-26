1e4c9342e120a46f5ff925d2d46249864a43d24d
signalwerk <sh@signalwerk.ch>
2018-12-26 05:42:24 +0100

    ADD: docs

---
+++ docs/precache-manifest.6c18b6201ec5a4874f46500200c56411.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0602a4227f05b0b06914",
+    "url": "/static/js/runtime~main.0602a422.js"
+  },
+  {
+    "revision": "025cdcbacdbfe39e04a6",
+    "url": "/static/js/main.025cdcba.chunk.js"
+  },
+  {
+    "revision": "86a955df7d49f140c118",
+    "url": "/static/js/4.86a955df.chunk.js"
+  },
+  {
+    "revision": "3993c02feaf95ec30b53",
+    "url": "/static/js/3.3993c02f.chunk.js"
+  },
+  {
+    "revision": "125ba993e6be8b389cf2",
+    "url": "/static/js/2.125ba993.chunk.js"
+  },
+  {
+    "revision": "b3580a9c69883211f5d1",
+    "url": "/static/js/1.b3580a9c.chunk.js"
+  },
+  {
+    "revision": "025cdcbacdbfe39e04a6",
+    "url": "/static/css/main.d2ef43f6.chunk.css"
+  },
+  {
+    "revision": "b3580a9c69883211f5d1",
+    "url": "/static/css/1.359bf033.chunk.css"
+  },
+  {
+    "revision": "c59a681e2bce326c4351f5d5c1179c66",
+    "url": "/index.html"
+  }
+];
