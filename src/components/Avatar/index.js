6e927e7cd62486cff378f69abe28972649dfc072
signalwerk <sh@signalwerk.ch>
2018-12-28 04:46:22 +0100

    ADD: print styles

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListItem, ListCol } from "../List";
+import { ListRoot, ListItem, ListCol } from "../List";
 import "./styles.css";
 
 import StefanHuber from "./img/StefanHuber1673k.jpg";
@@ -7,16 +7,18 @@ import Signalwerk from "./img/signalwerk.svg";
 
 const Avatar = ({ children }) => (
   <div className="avatar">
-    <ListItem>
-      <ListCol column={0}>
-        <div className="avatar--icon">
-          <img className="avatar--img" src={Signalwerk} alt="Stefan Huber" />
-        </div>
-      </ListCol>
-      <ListCol column={1}>
-        <img className="avatar--img" src={StefanHuber} alt="Stefan Huber" />
-      </ListCol>
-    </ListItem>
+    <ListRoot>
+      <ListItem>
+        <ListCol column={0}>
+          <div className="avatar--icon">
+            <img className="avatar--img" src={Signalwerk} alt="Stefan Huber" />
+          </div>
+        </ListCol>
+        <ListCol column={1}>
+          <img className="avatar--img" src={StefanHuber} alt="Stefan Huber" />
+        </ListCol>
+      </ListItem>
+    </ListRoot>
   </div>
 );
 
